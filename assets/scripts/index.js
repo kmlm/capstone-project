6692fcd2e7a5ffad46aa17b6a0873c4af7f7ef63
kmlm <kevin.mcaleese@libertymutual.com>
2017-11-25 14:22:52 -0500

    Widgets from StatsFc - Buttons / collapse

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,23 +3,13 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvents = require('./auth/events')
-const store = require('./store')
 
 $(() => {
   setAPIOrigin(location, config)
 })
 
-$('.team-choice').on('click', (event) => {
-  event.preventDefault()
-  // $('#ChooseTeam').hide()
-  let test = event.target.closest('tr')
-  console.log('test is', test)
-  let test2 = test.attr('data-id')
-  console.log('test 2 is', test2)
-  // store.favTeam = this.parent()
-  // console.log(store.favTeam)
-  })
 
+$('#next-fixture-button').on('click', ()=> $('#NextFixture').show())
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
