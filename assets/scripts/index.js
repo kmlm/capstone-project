c246da65d3f0d501287dc7044a6c5df345d51c69
kmlm <kevin.mcaleese@libertymutual.com>
2017-11-25 13:43:32 -0500

    Initial Work on Choose team
    Trouble accessing data-id on tr tag

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,10 +3,22 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvents = require('./auth/events')
+const store = require('./store')
 
 $(() => {
   setAPIOrigin(location, config)
 })
+
+$('.team-choice').on('click', (event) => {
+  event.preventDefault()
+  // $('#ChooseTeam').hide()
+  let test = event.target.closest('tr')
+  console.log('test is', test)
+  let test2 = test.attr('data-id')
+  console.log('test 2 is', test2)
+  // store.favTeam = this.parent()
+  // console.log(store.favTeam)
+  })
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
