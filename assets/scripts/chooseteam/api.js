1d66b920fdea64ec36258a24f658984ce5dcce1d
kmlm <kevin.mcaleese@libertymutual.com>
2017-11-27 16:18:19 -0500

    Initial Work on choose team

--- assets/scripts/chooseteam/api.js
+++ assets/scripts/chooseteam/api.js
@@ -2,11 +2,21 @@ const config = require('../config.js')
 const store = require('../store')
 
 
-const chooseTeam = function (data) {
-  console.log('api runs')
-
-}
-
+// const chooseTeam = function (data) {
+//   console.log('api runs')
+//     return $.ajax({
+//       url: config.apiOrigin + '/users/' + store.user.id,
+//       method: 'PATCH',
+//       headers: {
+//         Authorization: 'Token token=' + store.user.token
+//       },
+//       data: {
+//         user: {
+//           favorite_team: data
+//       }
+//     }
+//     })
+//   }
 
 module.exports = {
   chooseTeam
--- assets/scripts/chooseteam/events.js
+++ assets/scripts/chooseteam/events.js
@@ -9,12 +9,12 @@ const dashboard = require('../dashboard/events')
 const onChooseTeam = function (event) {
   console.log('click worked')
   event.preventDefault()
-  const team = $(this)[0].closest('tr'); store.team = $(team).attr('data-id')
-    // api.chooseTeam(store.team)
-      // .then(() =>
-      ui.chooseTeamSuccess(store.team)
-      dashboard.populateDashboard(store.team)
-      // .catch(ui.chooseTeamFailure)
+  const team = $(this)[0].closest('tr');
+  store.team = $(team).attr('data-id')
+    api.chooseTeam(store.team)
+      .then(() => ui.chooseTeamSuccess(store.team))
+      .then(() => dashboard.populateDashboard(store.team))
+      .catch(ui.chooseTeamFailure)
 }
 
 const onChangeTeam = function () {
@@ -23,14 +23,9 @@ const onChangeTeam = function () {
   store.team = null
 }
 
-// const testHide = function () {
-//   $('#Dashboard').hide()
-// }
-
 const chooseTeamHandlers = function () {
 $('.team-choice').on('click', onChooseTeam)
 $('#change-favorite-team').on('click', onChangeTeam)
-$('#game-tracker').on('click', testHide)
 }
 
 module.exports = {
