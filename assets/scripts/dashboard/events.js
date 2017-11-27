3b08e80df6b79c0cdbb4416844ab1b40238c636a
kmlm <kevin.mcaleese@libertymutual.com>
2017-11-27 11:58:28 -0500

    Refactored dashboard buttons
    Refactored so dashboard button toggles moved out of index.js

---
+++ assets/scripts/dashboard/events.js
@@ -0,0 +1,17 @@
+
+
+const dashboardHandlers = function () {
+
+// buttons for showing / hiding on dashboard
+$('#next-fixture-button').on('click', () => $('#NextFixture').toggle())
+$('#top-scorers-button').on('click', () => $('#Top-Scorers').toggle())
+$('#League-Table-button').on('click', () => $('#League-Table').toggle())
+$('#Form-button').on('click', () => $('#Form').toggle())
+$('#Fixtures-button').on('click', () => $('#Fixtures').toggle())
+// $('#match-centre-button').on('click', () => $('#MatchCentre').toggle())
+}
+
+
+module.exports = {
+  dashboardHandlers
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,28 +3,19 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvents = require('./auth/events')
-
+const dashboardEvents = require('./dashboard/events')
 $(() => {
   setAPIOrigin(location, config)
 })
 
 $(() => {
-  authEvents.authHandlers()
+  authEvents.authHandlers(),
+  dashboardEvents.dashboardHandlers()
   // ui.uiHandlers()
   // siteUi.uiHandlers()
 })
 
-$('#next-fixture-button').on('click', () => $('#NextFixture').toggle())
 
-$('#top-scorers-button').on('click', () => $('#Top-Scorers').toggle())
-
-$('#League-Table-button').on('click', ()=> $('#League-Table').toggle())
-
-$('#Form-button').on('click', () => $('#Form').toggle())
-
-$('#Fixtures-button').on('click', () => $('#Fixtures').toggle())
-
-$('#match-centre-button').on('click', () => $('#MatchCentre').toggle())
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
