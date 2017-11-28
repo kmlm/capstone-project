50e3e7c232d040e6e395c650ec3e5c29e9f06302
kmlm <kevin.mcaleese@libertymutual.com>
2017-11-28 09:52:36 -0500

    Able to change data-team when first choose team
    Need to refactor so can adjust with changing team and signing in as
    existing

--- assets/scripts/chooseteam/events.js
+++ assets/scripts/chooseteam/events.js
@@ -11,15 +11,26 @@ const onChooseTeam = function (event) {
   event.preventDefault()
   const team = $(this)[0].closest('tr');
   store.team = $(team).attr('data-id')
+  const html = "<hr class='featurette-divider'> <h2> Next Fixture </h2> <script src='//code.jquery.com/jquery-2.1.4.min.js'></script><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src='https://statsfc-4f51.kxcdn.com/widget/next-fixture-1.0.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'statsfc-next-fixture-js'));</script><div id='next-fixture-div' class='statsfc-next-fixture'data-key='bXBdah2OOD0LsTjdOIVNZ7cdIqHzM4WdQmcsLO_X'data-team='test'data-use-default-css='true'></div>"
+  const replaced = html.replace('test',store.team)
+  console.log(replaced)
+  $('#NextFixture').append(replaced)
     // api.chooseTeam(store.team)
       ui.chooseTeamSuccess(store.team)
-      dashboard.populateDashboard(store.team)
+      dashboard.populateDashboard(store.team
+    )
       // .catch(ui.chooseTeamFailure)
 }
 
 const onChangeTeam = function () {
   $('#Dashboard').hide()
   $('#ChooseTeam').show()
+  const html = "<hr class='featurette-divider'> <h2> Next Fixture </h2> <script src='//code.jquery.com/jquery-2.1.4.min.js'></script><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src='https://statsfc-4f51.kxcdn.com/widget/next-fixture-1.0.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'statsfc-next-fixture-js'));</script><div id='next-fixture-div' class='statsfc-next-fixture'data-key='bXBdah2OOD0LsTjdOIVNZ7cdIqHzM4WdQmcsLO_X'data-team='test'data-use-default-css='true'></div>"
+  const replaced = html.replace(store.team,'test')
+  console.log(replaced)
+  $('#NextFixture').replace(replaced)
+  // $('#NextFixture').append(replaced)
+
   store.team = null
 }
 
