e1b1cb9610742f36173c822e311135ca56abe99b
kmlm <kevin.mcaleese@libertymutual.com>
2017-11-28 14:42:06 -0500

    .html instead of .append for handlebar return

--- assets/scripts/gametracker/ui.js
+++ assets/scripts/gametracker/ui.js
@@ -4,18 +4,18 @@ const showGamesTemplate = require('../templates/game-index.handlebars')
 const getGamesSuccess = function (games) {
   store.games = games.games
   console.log(store.games)
-  const dateConvert = function (input) {
-    for (let i=1; i<input.length; i++)
-    {
-      store.games[i].date = input[i].date.split('T')[0].split('-').reverse().join('-')
-    }
-  }
-dateConvert(store.games)
-console.log(store.games)
-  const showGames = showGamesTemplate({
-    games: store.games
-  })
-  $('#GamesList').append(showGames)
+      const dateConvert = function (input) {
+        for (let i=1; i<input.length; i++)
+        {
+          store.games[i].date = input[i].date.split('T')[0].split('-').reverse().join('-')
+        }
+      }
+    dateConvert(store.games)
+    console.log(store.games)
+      const showGames = showGamesTemplate({
+        games: store.games
+      })
+      $('#GamesList').html(showGames)
 }
 
 const getGamesFailure = function () {
