627e7eb0def0c73063a90480d6760a45eb25d86f
kmlm <kevin.mcaleese@libertymutual.com>
2017-11-28 10:14:21 -0500

    Ongoing difficulty with changing team
    Appending the plug in to a section that is removed on change team, but
    having issues with choosing the new team

--- assets/scripts/chooseteam/events.js
+++ assets/scripts/chooseteam/events.js
@@ -11,10 +11,12 @@ const onChooseTeam = function (event) {
   event.preventDefault()
   const team = $(this)[0].closest('tr');
   store.team = $(team).attr('data-id')
+  const addedFixture = "<section id='added-fixture'></section>"
+  $('#NextFixture').append(addedFixture)
   const html = "<hr class='featurette-divider'> <h2> Next Fixture </h2> <script src='//code.jquery.com/jquery-2.1.4.min.js'></script><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src='https://statsfc-4f51.kxcdn.com/widget/next-fixture-1.0.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'statsfc-next-fixture-js'));</script><div id='next-fixture-div' class='statsfc-next-fixture'data-key='bXBdah2OOD0LsTjdOIVNZ7cdIqHzM4WdQmcsLO_X'data-team='test'data-use-default-css='true'></div>"
   const replaced = html.replace('test',store.team)
   console.log(replaced)
-  $('#NextFixture').append(replaced)
+  $('#added-fixture').append(replaced)
     // api.chooseTeam(store.team)
       ui.chooseTeamSuccess(store.team)
       dashboard.populateDashboard(store.team
@@ -23,14 +25,13 @@ const onChooseTeam = function (event) {
 }
 
 const onChangeTeam = function () {
+  $('#added-fixture').remove()
   $('#Dashboard').hide()
   $('#ChooseTeam').show()
-  const html = "<hr class='featurette-divider'> <h2> Next Fixture </h2> <script src='//code.jquery.com/jquery-2.1.4.min.js'></script><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src='https://statsfc-4f51.kxcdn.com/widget/next-fixture-1.0.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'statsfc-next-fixture-js'));</script><div id='next-fixture-div' class='statsfc-next-fixture'data-key='bXBdah2OOD0LsTjdOIVNZ7cdIqHzM4WdQmcsLO_X'data-team='test'data-use-default-css='true'></div>"
-  const replaced = html.replace(store.team,'test')
-  console.log(replaced)
-  $('#NextFixture').replace(replaced)
+  // const html = `<hr class='featurette-divider'> <h2> Next Fixture </h2> <script src='//code.jquery.com/jquery-2.1.4.min.js'></script><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src='https://statsfc-4f51.kxcdn.com/widget/next-fixture-1.0.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'statsfc-next-fixture-js'));</script><div id='next-fixture-div' class='statsfc-next-fixture'data-key='bXBdah2OOD0LsTjdOIVNZ7cdIqHzM4WdQmcsLO_X'data-team=${store.team} data-use-default-css='true'></div>`
+  // const replaced = html.replace(store.team,'test')
+  // console.log(replaced)
   // $('#NextFixture').append(replaced)
-
   store.team = null
 }
 
